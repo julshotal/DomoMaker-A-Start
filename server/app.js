c95866278e352ab67d5d85f06def1d963053edad
julshotal <jgh6364@g.rit.edu>
2019-10-31 12:47:27 -0400

    Fixed spelling errors

--- server/app.js
+++ server/app.js
@@ -63,8 +63,8 @@ app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
 
-app.user(csrf());
-app.user((err, req, res, next) => {
+app.use(csrf());
+app.use((err, req, res, next) => {
   if (err.code !== 'EBADCSRFTOKEN') return next(err);
 
   console.log('Missing CSRF token');
