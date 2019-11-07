2da29232ee211e8d7429bd05426f733b9b8757b5
julshotal <jgh6364@g.rit.edu>
2019-11-07 13:33:32 -0500

    css

--- client/app/maker.js
+++ client/app/maker.js
@@ -50,7 +50,7 @@ const DomoList = function(props) {
                 <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
                 <h3 className="domoName"> Name: {domo.name} </h3>
                 <h3 className="domoAge"> Age: {domo.age} </h3>
-                <p> My goal in life is: {domo.goal} </p>
+                <p className="domoGoal"> My goal in life is: {domo.goal} </p>
             </div>
         );
     });
