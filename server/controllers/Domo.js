c754f887f02ed05ebe02d9a1026eeeacc1aad96d
julshotal <jgh6364@g.rit.edu>
2019-11-07 12:30:13 -0500

    this is the worst

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -17,12 +17,12 @@ const makeDomo = (req, res) => {
   if (!req.body.name || !req.body.age || !req.body.cowboy) {
     return res.status(400).json({ error: 'All fields are required' });
   }
-  console.log(req.body.cowboy);
+  console.log(req.body.cowboy.val());
 
   const domoData = {
     name: req.body.name,
     age: req.body.age,
-    cowboy: req.body.cowboy,
+    cowboy: req.body.cowboy.val(),
     owner: req.session.account._id,
   };
 
