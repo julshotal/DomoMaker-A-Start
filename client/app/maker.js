aae887822c323fa8f122624022084be690db45f2
julshotal <jgh6364@g.rit.edu>
2019-11-07 12:48:01 -0500

    I would literally pay $100 right now for mongo to work on my laptop so I could stop this

--- client/app/maker.js
+++ client/app/maker.js
@@ -28,7 +28,7 @@ const DomoForm = (props) => {
             <input id="domoAge" type="text" name="age" placeholder="Domo Age"/>
             <label htmlFor="cowboy">Do you want to be a cowboy?</label>
             <select id="domoBoy" type="select" name="cowboy" size="2">
-                <option value="/assets/img/domofaceC.jpeg">I want to be a cowboy, baby (hell yeah)</option>
+                <option value="/assets/img/domofaceC.jpg">I want to be a cowboy, baby (hell yeah)</option>
                 <option value="/assets/img/domoface.jpeg">No</option>
             </select>
             <input type="hidden" name="_csrf" value={props.csrf} />
@@ -55,7 +55,7 @@ const DomoList = function(props) {
     const domoNodes = props.domos.map(function(domo) {
         return (
             <div key={domo._id} className="domo">
-                <img src="/assets/img/domofaceC.jpeg" alt="domo face" className="domoFace" />
+                <img src={domo.cowboy} alt="domo face" className="domoFace" />
                 <h3 className="domoName"> Name: {domo.name} </h3>
                 <h3 className="domoAge"> Age: {domo.age} </h3>
             </div>
